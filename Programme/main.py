270d34be1cb3ff306a068ecefb70d10be4b1663f
Taco <wangziqi2002vip@163.com>
2021-12-15 11:20:51 +0800

    taco did somethine

---
+++ Programme/main.py
@@ -0,0 +1,141 @@
+#SI100B 期末project-太阳能追踪
+#舵机控制实现追踪
+#光敏电阻读取电压比对方位
+#IOT平台接入
+#oled屏幕显示电压电流,方位等
+
+#引脚说明:
+#光敏电阻引脚: IO13 12 14 27
+#舵机控制引脚: IO15 2
+#oled屏幕引脚: IO16 17
+#太阳能电压  : IO25
+#太阳能电流  : IO26
+#变压输出电压: IO33
+#变压输出电流: IO32
+
+
+
+import machine
+import time
+import network
+from umqtt import simple as mqtt
+import _thread
+import ujson
+
+
+###阿里云IOT平台接入
+
+ALINK_PROP_SET_METHOD='thing.service.property.set'  #阿里云的东西
+
+def threadPublish():    #发布信息
+    while True:
+        time.sleep(2)
+        # d.measure()
+        # print(d.humidity())
+        # print(d.temperature())
+        # send_mseg={"params":{"Temperature":d.temperature(),"Humidity":d.humidity()},"method":"thing.service.property.set"}
+        client.publish(topic=" ",msg=str(send_mseg),qos=1,retain=False)
+
+def receiveMessage():   #接收信息
+    while True:
+        client.wait_msg()
+#接收信息。接收到的信息是json格式，要进行解析。
+def recvMessage(topic,msg):   #接收信息
+    # parsed=ujson.loads(msg)
+    # str=parsed["params"]
+    # print(str)
+    # print(type(parsed["params"]))
+    # print(str.get("PowerSwitch"))
+    # global state        
+    # state=str.get("PowerSwitch")
+    # if state == 1:
+    #     led.value(1)
+    #     print("led on!") 
+    # if state == 0:
+    #     led.value(0)
+    #     print("led off!")
+
+wlan=network.WLAN(network.STA_IF)  
+wlan.active(True)
+wlan.connect('HUAWEI Mate 40 Pro+','20020926')#连接WIFI
+ProductKey='gowkbf7kwnu'
+DeviceName='FNCSAGXVbwdR6kuQ'
+DeviceSecret='信导期末项目'
+CLIENT_ID='esp32'
+user_name='wzqvip'#用户名
+user_password='20020926Wang'#用户密码
+SERVER= "https://iot.console.aliyun.com/product/productDetail/gowkbf7kwnu"#阿里云物联网平台地址
+PORT=1883
+client = mqtt.MQTTClient(client_id=CLIENT_ID, server=SERVER, port=PORT, user=user_name, password=user_password, keepalive=60)
+client.connect()
+client.set_callback(recvMessage)#设置回调函数
+client.subscribe(" ")#订阅主题
+
+
+###主程序部分
+
+#先来读取电压吧
+def voltage_transfer(voltage):
+    return voltage/1024*3.3
+
+#四个光敏电阻的电压
+light_sensor1=machine.ADC(machine.Pin(13))
+light_sensor2=machine.ADC(machine.Pin(12))
+light_sensor3=machine.ADC(machine.Pin(14))
+light_sensor4=machine.ADC(machine.Pin(27))
+
+#把12设置为上下,34设置为左右
+analog_diff_up_and_down=light_sensor1-light_sensor2
+analog_diff_left_and_right=light_sensor3-light_sensor4
+
+#这里写一个速度控制吧,电压不管了
+def speed_calculate(diff):
+    if -100<diff<100:
+        return 0
+    elif 100<diff<500:
+        return 1
+    elif 500<diff<1000:
+        return 2
+    elif 1000<diff:
+        return 3
+    elif diff<-1000:
+        return -3
+    elif -1000<diff<-500:
+        return -2
+    elif -500<diff<-100:
+        return -1
+    else:
+        print("out of control")
+
+###舵机控制部分
+angle1 = 90  #舵机1初始位置
+angle2 = 90  #舵机2初始位置
+
+servo1 = machine.DAC(machine.Pin(15))
+servo2 = machine.DAC(machine.Pin(2))
+
+def angle_to_pwm(angle):
+    return int(angle*10/180+1500)
+
+def servo_move_to(angle,servo):
+    servo.write(angle_to_pwm(angle))
+
+def servo_control(servo,speed):
+    if speed == 0:
+        pass
+    elif speed==1:
+        servo.write(angle_to_pwm(angle1+1))
+    elif speed==-1:
+        servo.write(angle_to_pwm(angle1-1))
+    elif speed==2:
+        servo.write(angle_to_pwm(angle1+2))
+    elif speed==-2:
+        servo.write(angle_to_pwm(angle1-2))
+    elif speed==3:
+        servo.write(angle_to_pwm(angle1+3))
+    elif speed==-3:
+        servo.write(angle_to_pwm(angle1-3))
+    else:
+        print("out of control")
+
+        
