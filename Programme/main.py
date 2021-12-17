6f6392b4b0821287daf193e0a14be5d55385ef54
Taco <wangziqi2002vip@163.com>
2021-12-17 11:03:23 +0800

    控制写完了,还差阿里云
    应该能工作,不知道

--- Programme/main.py
+++ Programme/main.py
@@ -21,6 +21,7 @@ import network
 from umqtt import simple as mqtt
 import _thread
 import ujson
+import ssd1306
 
 
 ###阿里云IOT平台接入
@@ -74,9 +75,9 @@ client.subscribe(" ")#订阅主题
 
 ###主程序部分
 
-#先来读取电压吧
-def voltage_transfer(voltage):
-    return voltage/1024*3.3
+#先来读取电压吧   放到后面了
+# def voltage_transfer(voltage):
+#     return voltage/1024*3.3
 
 #四个光敏电阻的电压
 light_sensor1=machine.ADC(machine.Pin(13))
@@ -96,6 +97,7 @@ def speed_calculate(diff):
         return diff/100
 
 ###舵机控制部分
+global angle1,angle2
 angle1 = 90  #舵机1初始位置
 angle2 = 90  #舵机2初始位置
 
@@ -110,13 +112,86 @@ def servo_move_to(angle,servo):
 
 def servo_control(servo,speed):
     global angle1,angle2
-    if servo == 1:
-        servo_move_to(angle1+speed,servo1)
-        angle1 = angle1+speed
-    if servo == 2:
-        servo_move_to(angle2+speed,servo2)
-        angle2 = angle2+speed
-    else:
-        print("No motor")
+    if servo == servo1:
+        angle1 += speed
+        if angle1 > 180:  #极限复位
+            angle1 = 0
+        if angle1 < 0:
+            angle1 = 180
+        servo_move_to(angle1,servo)
+    if servo == servo2:
+        angle2 += speed
+        if angle2 > 180:   #这里没啥,防止报错用的
+            angle2 = 180
+        elif angle2 < 0:
+            angle2 = 0
+        servo_move_to(angle2,servo)
+
 
 ###接下来是oled屏幕显示
+oled_pins = machine.I2C(scl=machine.Pin(16), sda=machine.Pin(17))
+oled_display = ssd1306.SSD1306_I2C(128, 64, oled_pins)
+
+def display_oled(text1,text2,text3,text4):
+    oled_display.fill(0)
+    oled_display.text(text1, 0, 0)
+    oled_display.text(text2, 0, 10)
+    oled_display.text(text3, 0, 20)
+    oled_display.text(text4, 0, 30)
+    oled_display.show()
+
+#oled_display.invert(True)  #反色显示
+
+##然后是一些简易的电压换算之类的.
+
+sun_voltage=machine.ADC(machine.Pin(25))
+sun_current=machine.ADC(machine.Pin(26))
+stable_voltage=machine.ADC(machine.Pin(33))
+stable_current=machine.ADC(machine.Pin(32))
+
+def voltage_transfer(voltage):
+    return voltage/1024*3.3
+
+def read_voltage_sun():
+    return voltage_transfer(sun_voltage)
+    
+def read_current_sun():
+    return voltage_transfer(sun_current)*1000/185  #单位换算: 185mv/A
+
+def read_voltage_stable():
+    return voltage_transfer(stable_voltage)
+
+def read_current_stable():
+    return voltage_transfer(stable_current)*1000/185  #单位换算: 185mv/A
+
+
+###主程序?
+def main_control():
+    #首先确认旋转方向
+    global angle1,angle2
+    speed1=speed_calculate(analog_diff_left_and_right)
+    speed2=speed_calculate(analog_diff_up_and_down)
+    #然后是电机控制
+    servo_control(servo1,speed1)
+    servo_control(servo2,speed2)
+
+def main_display0():
+    line1 = str("Sun auto follower")
+    line2 = str(angle1+"°"+" "+angle2+"°")
+    line3 = str("V1"+str(read_voltage_sun())+"V"+" I1"+str(read_current_sun())+"A")
+    line4 = str("V2"+str(read_voltage_stable())+"V"+" I2"+str(read_current_stable())+"A")
+    display_oled(line1,line2,line3,line4)
+
+# def main_display1():
+#     line1="Sun auto follower"
+#     line2=
+
+
+
+def main_loop():
+    while True:
+        main_control()
+        main_display0()
+        time.sleep(0.1)
+
+main_loop()
